6dcb74d9a760574e37bbfab2130377d48c7b4da0
Yasemin <uysalyy96@gmail.com>
2021-02-14 11:43:47 +0300

    Hover effect

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
+    div.classList.add('grid');
+}
+
+const cells = document.querySelectorAll('.grid');
+
+cells.forEach(cell => cell.addEventListener('mouseover', changeColor));
+
+function changeColor() {
+    this.classList.add('color');
+    console.log(this);
 }
