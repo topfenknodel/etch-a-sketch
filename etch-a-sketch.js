9530fb223ee6febbfb7107a86217c83e71641584
Yasemin <uysalyy96@gmail.com>
2021-02-16 12:02:26 +0300

    Clear grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,10 +7,28 @@ for (let i = 0; i < 256; i++) {
 }
 
 const cells = document.querySelectorAll('.grid');
+cells.forEach(cell => cell.addEventListener('mouseover', randomColor));
 
-cells.forEach(cell => cell.addEventListener('mouseover', changeColor));
+//function changeColor() {
+//    this.classList.add('color');
+//    console.log(this);
+//}
 
-function changeColor() {
-    this.classList.add('color');
-    console.log(this);
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let a = 0.9;
+
+    let color = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+
+    this.style.backgroundColor = color;
 }
+
+const clButton = document.querySelector('#clear');
+clButton.addEventListener('click', clearGrid);
+
+function clearGrid() {
+    cells.forEach(cell => cell.style.backgroundColor = "transparent");
+}
+
